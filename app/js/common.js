82cd26726e271f212c65f6c7407b3389c7da6d8f
alex <alex_kobilianskiy@bigmir.net>
2019-12-08 15:44:44 +0200

    section begin complete

--- app/js/common.js
+++ app/js/common.js
@@ -35,6 +35,21 @@ $(document).ready(function(){
     <!-- /javascript -->
 
 
+    var uPhone = $('.user-phone');
+    uPhone.mask("+7 (999) 999-99-99",{autoclear: false});
+
+    uPhone.on('click', function (ele) {
+        var needelem = ele.target || event.srcElement;
+        needelem.setSelectionRange(4,4);
+        needelem.focus();
+    });
+
+    $.validate({
+        form : '.contact-form',
+        scrollToTopOnError: false
+    });
+
+
     //E-mail Ajax Send
     $("form").submit(function() { //Change
         var th = $(this);
