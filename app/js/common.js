b547682613de42b47423a0271057ef69a24f3e58
alex <alex_kobilianskiy@bigmir.net>
2019-12-09 17:24:08 +0200

    section cases complete

--- app/js/common.js
+++ app/js/common.js
@@ -35,6 +35,99 @@ $(document).ready(function(){
     <!-- /javascript -->
 
 
+    function casesMeta (item){
+        var metaImg = item.data('adimg'),
+            metaPreview = item.data('previewname'),
+            metaClickBefore = item.data('clickbefore'),
+            metaClickAfter = item.data('clickafter'),
+            metaClaimBefore = item.data('claimbefore'),
+            metaClaimAfter = item.data('claimafter');
+
+        $('#advert-img').attr('src', metaImg);
+        $('#click-before').text(metaClickBefore);
+        $('#click-after').text(metaClickAfter);
+        $('#claim-before').text(metaClaimBefore);
+        $('#claim-after').text(metaClaimAfter);
+
+    }
+
+
+    /** CASES FUNCTIONALITY start **/
+    $('.cases-slider').on('initialized.owl.carousel', function (e) {
+        var firstSlide = $('.cases-slider .owl-item.active');
+
+        var currentSlideIndex = e.item.index + 1;
+        var currentSlide = $(".cases-slider .owl-item:nth-child("+currentSlideIndex+")");
+        var currentItem = currentSlide.find('.case-slide');
+
+        var prevSlide = $(".cases-slider .owl-item:nth-child("+(currentSlideIndex-1)+")");
+        var prevItem = prevSlide.find('.case-slide');
+        var prevItemPreview = prevItem.data('previewname');
+
+        var nextSlide = $(".cases-slider .owl-item:nth-child("+(currentSlideIndex+1)+")");
+        var nextItem = nextSlide.find('.case-slide');
+        var nextItemPreview = nextItem.data('previewname');
+
+
+
+        casesMeta(currentItem);
+        $("#cases-prev-name").text(prevItemPreview);
+        $("#cases-next-name").text(nextItemPreview);
+
+        currentSlide.addClass('explice');
+    });
+
+    var $casesSlider = $('.cases-slider').owlCarousel({
+        loop: true,
+        nav: false,
+
+        margin: 70,
+        dots: false,
+        autoHeight: false,
+        navText: ["",""],
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items: 1,
+            },
+            // breakpoint from 992 up
+            992 : {
+                items: 2
+            }
+        }
+    });
+
+    $('.cases-prev').click(function() {
+        $casesSlider.trigger('prev.owl.carousel');
+    });
+
+    $('.cases-next').click(function() {
+        $casesSlider.trigger('next.owl.carousel');
+    });
+
+    $casesSlider.on('changed.owl.carousel', function (e) {
+        $('.cases-slider .owl-item').removeClass('explice');
+        var currentSlideIndex = e.item.index + 1;
+        console.log(currentSlideIndex);
+        var currentSlide = $(".cases-slider .owl-item:nth-child("+currentSlideIndex+")");
+        var nextSlide = $(".cases-slider .owl-item:nth-child("+(currentSlideIndex+1)+")");
+        var prevSlide = $(".cases-slider .owl-item:nth-child("+(currentSlideIndex-1)+")");
+        var currentItem = currentSlide.find('.case-slide');
+        var nextItem = nextSlide.find('.case-slide');
+        var prevItem = prevSlide.find('.case-slide');
+        var nextItemPreview = nextItem.data('previewname');
+        var prevItemPreview = prevItem.data('previewname');
+
+        currentSlide.addClass('explice');
+
+        casesMeta(currentItem);
+
+        $("#cases-next-name").text(nextItemPreview);
+        $("#cases-prev-name").text(prevItemPreview);
+
+    });
+    /** CASES FUNCTIONALITY end **/
+
     function heightses() {
         if ($(window).width()>=992) {
             $('.price-item-top').matchHeight();
--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,7 @@ gulp.task('scripts', function() {
 		'app/libs/inputmask/jquery.maskedinput.min.js',
 		'app/libs/jquery-form-validator/form-validator/jquery.form-validator.min.js',
 		'app/libs/matchHeight/jquery.matchHeight-min.js',
+		'app/libs/owl-carousel/owl.carousel.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
